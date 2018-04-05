e7f48f5d0eebedcc6ddbee40d94fca209ed2192b
Tobias Ammann <tobias.ammann@adnm.ch>
2018-04-05 17:04:55 +0200

    Update tests for execution on travis

--- tests/test_exoscale_dns.py
+++ tests/test_exoscale_dns.py
@@ -9,64 +9,95 @@ import os
 import time
 
 
+################################################################################
+# Fixtures
+################################################################################
+
 @pytest.fixture
 def access_key():
     return exoscale_dns.access_key(
-        os.environ.get('APIKEY'),
-        os.environ.get('SECRET')
+        os.environ.get('APIKEY', 'invalid'),
+        os.environ.get('SECRET', 'invalid')
     )
 
+@pytest.fixture
+def test_domain(access_key):
+    for domain in access_key:
+        if domain.name == os.environ.get('DOMAIN', 'invalid'):
+            yield domain
+            break
+            try:
+                domain.delete()
+            except:
+                pass
+    else:
+        domain = access_key.create(os.environ.get('DOMAIN', 'invalid'))
+        yield domain
+        try:
+            domain.delete()
+        except:
+            pass
+
+
+@pytest.fixture
+def test_record(test_domain):
+    for record in test_domain:
+        if record.name.startswith('test'):
+            return record
+    return test_domain.add_record(f'test{int(time.time())}', 'TXT', 'test')
+
+
+################################################################################
+# Tests for `access_key`
+################################################################################
 
 def test_create_access_key(access_key):
-    """Sample pytest test function with the pytest fixture as an argument."""
-    # from bs4 import BeautifulSoup
-    # assert 'GitHub' in BeautifulSoup(response.content).title.string
     assert access_key is not None
 
 def test_list_all_domains(access_key):
     for domain in sorted(access_key):
         assert isinstance(domain, exoscale_dns.domain)
 
-def test_list_all_records(access_key):
-    for domain in sorted(access_key):
-        for record in domain:
-            assert isinstance(record, exoscale_dns.record)
-        break
 
-def test_fetch_zone_file(access_key):
-    for domain in sorted(access_key):
-        zone = domain.zone()
-        assert isinstance(zone, str)
-        assert len(zone) > 0
-        break
+################################################################################
+# Tests for `domain`
+################################################################################
 
-def test_add_record(access_key):
-    for domain in sorted(access_key):
-        record = domain.add_record(f'test{int(time.time())}', 'TXT', 'test')
+def test_create_test_domain(test_domain):
+    assert test_domain is not None
+
+def test_list_all_records(test_domain):
+    for record in test_domain:
         assert isinstance(record, exoscale_dns.record)
-        break
 
-def test_update_record(access_key):
-    for domain in sorted(access_key):
-        for record in domain:
-            if record.name.startswith('test'):
-                record2 = record.update(content=f'{time.time()}')
-                assert record.content != record2.content
-                assert record.id == record2.id
-        break
+def test_fetch_zone_file(test_domain):
+    zone = test_domain.zone()
+    assert isinstance(zone, str)
+    assert len(zone) > 0
 
-def test_delete_record(access_key):
-    for domain in sorted(access_key):
-        for record in domain:
-            if record.name.startswith('test'):
-                assert record.delete() == None
-        break
+def test_delete_domain(test_domain, access_key):
+    assert test_domain.delete() is None
+    time.sleep(5)
+    for domain in access_key:
+        assert domain.name != test_domain.name
 
-def test_create_delete_domain(access_key):
-    name = f'test-domain-{int(time.time())}.ch'
-    domain = access_key.create(name)
-    assert isinstance(domain, exoscale_dns.domain)
-    domain.delete()
-    time.sleep(3)
-    for d in list(access_key):
-        assert d.name != name
+
+
+################################################################################
+# Tests for `record`
+################################################################################
+
+def test_add_record(test_domain):
+    record = test_domain.add_record(f'test{int(time.time())}', 'TXT', 'test')
+    assert isinstance(record, exoscale_dns.record)
+
+def test_update_record(test_record):
+    record2 = test_record.update(content=f'{time.time()}')
+    assert test_record.content != record2.content
+    assert test_record.id == record2.id
+
+def test_delete_record(test_record, test_domain):
+    assert test_record.delete() is None
+    time.sleep(5)
+    for record in test_domain:
+        assert record.name != test_record.name
